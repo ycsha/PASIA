148e2bab2b7ede6295001b44296c65ac7a586572
ycsha <149214372+ycsha@users.noreply.github.com>
2024-05-18 22:54:49 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.toggle('active');
+}
+
+let searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () =>{
+    navbar.classList.toggle('active');
+}
+
